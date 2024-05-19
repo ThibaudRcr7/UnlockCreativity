b920140f19ae241a14316edd0689a66ebc31b6d4
ThibaudR <thibaudrcr@gmail.com>
2024-05-19 15:00:36 +0200

    fichier structure + état actif header

--- src/js/app.js
+++ src/js/app.js
@@ -4,40 +4,58 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import $ from 'jquery';
-
-
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import $ from "jquery";
 
 gsap.registerPlugin(ScrollTrigger);
 
-'use strict';
+("use strict");
 
 // HEADER NAV - SCROLL :
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   let lastScrollTop = 0;
-  const navbar = document.querySelector('.header'); // Assurez-vous que le sélecteur correspond à votre HTML
+  const navbar = document.querySelector(".header"); // Assurez-vous que le sélecteur correspond à votre HTML
 
-  window.addEventListener('scroll', () => {
-      let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  window.addEventListener("scroll", () => {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-      if (scrollTop > lastScrollTop) {
-          // Scrolling vers le bas
-          navbar.style.top = '-100%'; // Adaptez cette valeur selon la hauteur de votre barre de navigation
-      } else {
-          // Scrolling vers le haut
-          navbar.style.top = '0';
-      }
-      lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Pour éviter les valeurs négatives
+    if (scrollTop > lastScrollTop) {
+      // Scrolling vers le bas
+      navbar.style.top = "-100%"; // Adaptez cette valeur selon la hauteur de votre barre de navigation
+    } else {
+      // Scrolling vers le haut
+      navbar.style.top = "0";
+    }
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Pour éviter les valeurs négatives
   });
+});
+
+// ETAT ACTIF : HEADER
+
+document.addEventListener('DOMContentLoaded', () => {
+  const currentPath = window.location.pathname;
+
+  const links = {
+      '/index.html': 'index-link',
+      '/projets.html': 'projets-link',
+      '/stage.html': 'stage-link',
+      '/contact.html': 'contact-link',
+  };
+
+  for (const path in links) {
+      if (currentPath.endsWith(path)) {
+          document.getElementById(links[path]).classList.add('active');
+          break;
+      }
+  }
 });
 
 // CARDS : PARALLAX
 
 let timeln = gsap.timeline({
   scrollTrigger: {
-    trigger: ".container-projets",
+    trigger: ".container-cards",
     pin: true,
     pinSpacing: true,
     start: "top-=180px top",
@@ -97,10 +115,13 @@ timeln.to(".card--3", {
 timeln.to(".card--3", {});
 
 
-// Scrolltrigger : section title 
+// Scrolltrigger : Img Talents
 
-document.addEventListener("DOMContentLoaded", function() {
-  const section = document.querySelector('.section-title');
+
+// Scrolltrigger : section title
+
+document.addEventListener("DOMContentLoaded", function () {
+  const section = document.querySelector(".section-title");
 
   gsap.fromTo(
     "#text-behind, #text-behind-blur, #text-front",
@@ -112,141 +133,185 @@ document.addEventListener("DOMContentLoaded", function() {
         trigger: section,
         start: "top bottom",
         end: "bottom top",
-        scrub: true,        
-      }
+        scrub: true,
+      },
     }
   );
 });
-
-
 
 // Scrolltrigger : wings
 
 gsap.to(".section-projets--img", {
   scrollTrigger: {
-    trigger: ".section-projets", 
-    start: "top top", 
+    trigger: ".section-projets",
+    start: "top top",
     end: "bottom top",
-    scrub: true
+    scrub: true,
   },
-  y: "150%", 
-  opacity: 0 
+  y: "150%",
+  opacity: 0,
 });
 
-// RANDOM MESSENGER 
+// RANDOM MESSENGER
 
-var Messenger = function(el){
+var Messenger = function (el) {
   var m = this;
-  
-  m.init = function(){
+
+  m.init = function () {
     m.codeletters = "壁に耳あり障子に目あり";
     m.message = 0;
     m.current_length = 0;
     m.fadeBuffer = false;
-    m.messages = [
-      'PASSION',
-      'CRÉATION',
-      'AMBITION'
-    ];
-    
+    m.messages = ["PASSION", "CRÉATION", "AMBITION"];
+
     setTimeout(m.animateIn, 500);
   };
-  
-  m.generateRandomString = function(length){
-    var random_text = '';
-    var reduced_length = Math.floor(length / 2); // Ajoutez cette ligne pour réduire la taille par 2
-    while(random_text.length < reduced_length){ // Modifiez cette ligne pour utiliser reduced_length
-      random_text += m.codeletters.charAt(Math.floor(Math.random()*m.codeletters.length));
-    } 
-      
+
+  m.generateRandomString = function (length) {
+    var random_text = "";
+    var reduced_length = Math.floor(length);
+    while (random_text.length < reduced_length) {
+
+      random_text += m.codeletters.charAt(
+        Math.floor(Math.random() * m.codeletters.length)
+      );
+    }
+
     return random_text;
   };
-  
-  
-  m.animateIn = function(){
-    if(m.current_length < m.messages[m.message].length){
+
+  m.animateIn = function () {
+    if (m.current_length < m.messages[m.message].length) {
       m.current_length = m.current_length + 2;
-      if(m.current_length > m.messages[m.message].length) {
+      if (m.current_length > m.messages[m.message].length) {
         m.current_length = m.messages[m.message].length;
       }
-      
+
       var message = m.generateRandomString(m.current_length);
       $(el).html(message);
-      
+
       setTimeout(m.animateIn, 300);
-    } else { 
+    } else {
       setTimeout(m.animateFadeBuffer, 300);
     }
   };
-  
-  m.animateFadeBuffer = function(){
-    if(m.fadeBuffer === false){
+
+  m.animateFadeBuffer = function () {
+    if (m.fadeBuffer === false) {
       m.fadeBuffer = [];
-      for(var i = 0; i < m.messages[m.message].length; i++){
-        m.fadeBuffer.push({c: (Math.floor(Math.random()*20))+1, l: m.messages[m.message].charAt(i)}); // Réduisez le nombre de cycles aléatoires
+      for (var i = 0; i < m.messages[m.message].length; i++) {
+        m.fadeBuffer.push({
+          c: Math.floor(Math.random() * 20) + 1,
+          l: m.messages[m.message].charAt(i),
+        }); // Réduisez le nombre de cycles aléatoires
       }
     }
-    
+
     var do_cycles = false;
-    var message = ''; 
-  
-    for(var i = 0; i < m.fadeBuffer.length; i++){
+    var message = "";
+
+    for (var i = 0; i < m.fadeBuffer.length; i++) {
       var fader = m.fadeBuffer[i];
-      if(fader.c > 0){
+      if (fader.c > 0) {
         do_cycles = true;
         fader.c--;
-        message += m.codeletters.charAt(Math.floor(Math.random()*m.codeletters.length));
+        message += m.codeletters.charAt(
+          Math.floor(Math.random() * m.codeletters.length)
+        );
       } else {
         message += fader.l;
       }
     }
-  
+
     $(el).html(message);
-    
-    if(do_cycles === true){
+
+    if (do_cycles === true) {
       setTimeout(m.animateFadeBuffer, 80);
     } else {
       setTimeout(m.cycleText, 2000);
     }
   };
-  
-  
-  m.cycleText = function(){
+
+  m.cycleText = function () {
     m.message = m.message + 1;
-    if(m.message >= m.messages.length){
+    if (m.message >= m.messages.length) {
       m.message = 0;
     }
-    
+
     m.current_length = 0;
     m.fadeBuffer = false;
-    $(el).html('');
-    
+    $(el).html("");
+
     setTimeout(m.animateIn, 200);
   };
-  
+
   m.init();
+};
+
+var messenger = new Messenger($("#messenger"));
+
+// CARD ABOUT : 3D TILT ANIM
+
+const about = document.querySelector(".container-about");
+if (about) {
+const defaultPosition =
+  "perspective(1000px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)";
+
+  const updateTilt = (x, y) => {
+    const containerRect = about.getBoundingClientRect();
+    const mouseX = x - containerRect.left;
+    const mouseY = y - containerRect.top;
+    const centerX = containerRect.width / 2;
+    const centerY = containerRect.height / 2;
+    const maxTilt = 15; // Adjust the maximum tilt angle as needed
+  
+    let tiltX = ((mouseX - centerX) / centerX) * maxTilt;
+    let tiltY = ((centerY - mouseY) / centerY) * maxTilt;
+  
+    tiltX = Math.min(Math.max(tiltX, -maxTilt), maxTilt);
+    tiltY = Math.min(Math.max(tiltY, -maxTilt), maxTilt);
+  
+    about.style.transform = `perspective(1000px) rotateX(${tiltY}deg) rotateY(${tiltX}deg) scale3d(1, 1, 1)`;
+  };
+  
+
+  let isTouchDevice = "ontouchstart" in window || navigator.maxTouchPoints > 0;
+
+  if (isTouchDevice) {
+
+
+    about.addEventListener("touchmove", (e) => {
+      const touch = e.touches[0];
+      updateTilt(touch.clientX, touch.clientY);
+    });
+    about.addEventListener("touchend", () => (about.style.transform = defaultPosition));
+  } else {
+    about.addEventListener("mousemove", (e) => updateTilt(e.clientX, e.clientY));
+    about.addEventListener("mouseleave", () => (about.style.transform = defaultPosition));
+  }
 }
 
-var messenger = new Messenger($('#messenger'));
 
 
 // THREE JS ANIM
 
 var renderer1, renderer2, scene, scene1, scene2, camera1, camera2;
+var canvas1 = document.getElementById("canvas-1");
+if (canvas1) {
+  renderer1 = new THREE.WebGLRenderer({
+    canvas: canvas1,
+    antialias: true,
+    alpha: true,
+  });
 
-renderer1 = new THREE.WebGLRenderer({
-  canvas: document.getElementById("canvas-1"),
-  antialias: true,
-  alpha: true
-});
-
-renderer1.setPixelRatio(window.devicePixelRatio);
-renderer1.setSize(window.innerWidth, window.innerHeight);
+  renderer1.setPixelRatio(window.devicePixelRatio);
+  renderer1.setSize(window.innerWidth, window.innerHeight);
+}
 
 renderer2 = new THREE.WebGLRenderer({
   canvas: document.getElementById("canvas-2"),
   antialias: true,
-  alpha: true
+  alpha: true,
 });
 
 renderer2.setPixelRatio(window.devicePixelRatio);
@@ -266,11 +331,21 @@ const hdrEquirect = new RGBELoader()
     scene2.environment = hdrEquirect;
   });
 
-camera1 = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera1 = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 camera1.position.z = 10;
 scene1.add(camera1);
 
-camera2 = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera2 = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 camera2.position.z = 10;
 scene2.add(camera2);
 
@@ -289,12 +364,12 @@ const material1 = new THREE.MeshStandardMaterial({
   color: 0xff0000,
   roughness: 0,
   metalness: 0.6,
-  envMapIntensity: 10
+  envMapIntensity: 10,
 });
 
 const gltfLoader = new GLTFLoader();
 gltfLoader.load(
-  '../images/key.glb',
+  "../images/key.glb",
   function (gltf) {
     console.log("Model loaded successfully");
     const object = gltf.scene;
@@ -303,34 +378,35 @@ gltfLoader.load(
         child.material = material1;
       }
     });
-    object.scale.setScalar(0.25);  
-    object.position.set(0, -1.4, 0);  
+    object.scale.setScalar(0.25);
+    object.position.set(0, -1.4, 0);
     group.add(object);
-    myObject = object;  
+    myObject = object;
 
-    gsap.fromTo(myObject.scale, 
-      { x: 0.25, y: 0.25, z: 0.25 }, 
+    gsap.fromTo(
+      myObject.scale,
+      { x: 0.25, y: 0.25, z: 0.25 },
       {
-        x: 0.45,  
-        y: 0.45, 
+        x: 0.45,
+        y: 0.45,
         z: 0.45,
         scrollTrigger: {
-          trigger: '.section-title',
-          start: 'top bottom',
-          end: 'bottom top',
-          scrub: true
-        }
+          trigger: ".section-title",
+          start: "top bottom",
+          end: "bottom top",
+          scrub: true,
+        },
       }
     );
   },
   undefined,
   function (error) {
-    console.error('An error happened loading the model:', error);
+    console.error("An error happened loading the model:", error);
   }
 );
 
 gltfLoader.load(
-  '../images/logo.glb',
+  "../images/logo.glb",
   function (gltf) {
     console.log("Deuxième modèle chargé avec succès");
     const secondObject = gltf.scene;
@@ -339,14 +415,17 @@ gltfLoader.load(
         child.material = material1;
       }
     });
-    secondObject.scale.setScalar(1);  
+    secondObject.scale.setScalar(1);
     secondObject.position.set(0, 0, 0);
     scene2.add(secondObject);
     mySecondObject = secondObject;
   },
   undefined,
   function (error) {
-    console.error('Une erreur s\'est produite lors du chargement du deuxième modèle:', error);
+    console.error(
+      "Une erreur s'est produite lors du chargement du deuxième modèle:",
+      error
+    );
   }
 );
 
@@ -355,7 +434,9 @@ window.addEventListener("resize", onWindowResize);
 function onWindowResize() {
   camera1.aspect = window.innerWidth / window.innerHeight;
   camera1.updateProjectionMatrix();
-  renderer1.setSize(window.innerWidth, window.innerHeight);
+  if (canvas1) {
+    renderer1.setSize(window.innerWidth, window.innerHeight);
+  }
 
   const nouvelleLargeur = window.innerWidth / 2;
   const nouvelleHauteur = window.innerHeight;
@@ -374,16 +455,15 @@ function update1() {
   camera1.lookAt(0, 0, 0);
 }
 
-function update2() {
-
-}
+function update2() {}
 
 function animate1() {
   requestAnimationFrame(animate1);
   update1();
-  renderer1.render(scene1, camera1);
+  if (canvas1) {
+    renderer1.render(scene1, camera1);
+  }
 }
-
 function animate2() {
   requestAnimationFrame(animate2);
   update2();
@@ -396,23 +476,17 @@ animate2();
 console.log("test");
 
 const radius = 30;
-const duration = 20; 
+const duration = 20;
 
-// gsap.to({}, {
-//   duration: duration,
-//   repeat: -1,
-//   onUpdate: function() {
-
-//     const t = this.progress();
-
-//     const radian = Math.PI * 2 * t;
-
-
-//     const y = Math.cos(radian) * radius;
-//     const z = Math.sin(radian) * radius;
-
-//     camera2.position.set(y, camera2.position.x, z);
-
-//     camera2.lookAt(0, 0, 0);
-//   }
-// });
+ gsap.to({}, {
+   duration: duration,
+   repeat: -1,
+   onUpdate: function() {
+     const t = this.progress();
+     const radian = Math.PI * 2 * t;
+     const y = Math.cos(radian) * radius;
+     const z = Math.sin(radian) * radius;
+     camera2.position.set(y, camera2.position.x, z);
+     camera2.lookAt(0, 0, 0);
+   }
+ });
